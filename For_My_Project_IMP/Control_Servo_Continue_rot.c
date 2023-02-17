44229e7ab5b0d41c015b4c320449b3d9e4a0a339
Irimia Dorin <42108589+Dorin-Irimia@users.noreply.github.com>
2023-02-17 11:18:06 +0200

    v1.2

--- For_My_Project_IMP/Control_Servo_Continue_rot.c
+++ For_My_Project_IMP/Control_Servo_Continue_rot.c
@@ -20,3 +20,47 @@ int main()
     }
     return 0;
 }
+
+
+/*********************** This code is at the enternet *********************************/
+
+#include <Servo.h>
+
+Servo myservo; 
+
+const int servoStop = 1500;
+const int servoSpeed = 50; 
+int potVal;
+int potStop;
+unsigned int servoStart;
+
+void setup() { 
+  
+  myservo.attach(6);
+  
+}
+
+void loop() {
+  
+  potVal = map (analogRead(0), 0, 1023, 0, 65000);  
+  
+    if (potVal > potStop) { 
+      int potSpeed = potStop - potVal; 
+      servoStart = servoStop + (servoSpeed + potSpeed); 
+      myservo.writeMicroseconds(servoStart); 
+    }
+  
+    else if (potVal < potStop) {   
+      int potSpeed = potStop - potVal;
+      servoStart = servoStop - (servoSpeed - potSpeed);
+      myservo.writeMicroseconds(servoStart); 
+    }
+  
+    else myservo.writeMicroseconds(servoStop);
+  
+  potStop = potVal; 
+  servoStart = 0;
+  
+  delay(15);  
+                         
+}
